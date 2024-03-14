242cbf59b1b6ba21e40d19b224ae5870c8c962a8
Mpincai-valencia <127371288+Mpincai-valencia@users.noreply.github.com>
2024-03-14 18:27:47 -0500

    Se actualizaron los datos del sync

--- STIC/src/DataBase/sync.js
+++ STIC/src/DataBase/sync.js
@@ -17,4 +17,12 @@ async function sync(){
     spent.belongsTo(batch,{
         foreignKey: 'batchId'
     });
+    await connection.sync({force: false})
+    .then(() => { 
+        console.log('Synchronized DataBase');
+    })
+    .catch((error) => { 
+        console.error('Error syncing DataBase' + error);
+    }); 
 }
+sync();
