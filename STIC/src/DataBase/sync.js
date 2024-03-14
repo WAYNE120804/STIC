e5254d62a4ce39a7849b7b8c3e6a7097f4b1b8f8
Mpincai-valencia <127371288+Mpincai-valencia@users.noreply.github.com>
2024-03-14 18:26:57 -0500

    Se modifico el sync y el server con las correciones de clase

--- STIC/src/DataBase/sync.js
+++ STIC/src/DataBase/sync.js
@@ -5,7 +5,16 @@ const farm = require('../Models/farm');
 const warehouse = require('../Models/warehouse');
 const contractor= require('../Models/contractor');
 const batch=require('../Models/batch');
+const spent= require('../Models/spent');
 
-function sync(){
-    
+async function sync(){
+    //Foreign Key batch - spent
+    batch.hasMany(spent,{
+        foreignKey: 'batchId',
+        onDelete: 'restrict',
+        onUpdate:'cascade'
+    });
+    spent.belongsTo(batch,{
+        foreignKey: 'batchId'
+    });
 }
--- STIC/src/server.js
+++ STIC/src/server.js
@@ -15,16 +15,9 @@ const spentrouter=require('./Routers/spentrouter.js');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-connection.sync({force: false})
-    .then(()=>{
-        console.log('Base de datos sincronizada');
-        app.listen(port, ()=>{
-            console.log("la aplicacion corre en el puerto: "+ port);
-        })
-    })
-    .catch((error)=>{
-        console.error('Error al sincronizar la base de datos:', error);
-    });
+app.listen(port, ()=>{
+     console.log("la aplicacion corre en el puerto: "+ port);
+})
 
 
 //api
